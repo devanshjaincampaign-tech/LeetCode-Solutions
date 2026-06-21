910db7e587bbe17de8a85c07f42f50c4b61ee870
Devansh jain <devanshjaincampaign@gmail.com>
2026-06-21 22:38:44 +0530

    Time: 2 ms (33.19%), Space: 10.6 MB (55.47%) - LeetHub

--- LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
+++ LeetCode/Easy/0338-counting-bits/0338-counting-bits.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> bits(n + 1);
-
-        for (int i = 0; i <= n; i++) {
-            int a = i;
-            int count = 0;
-
-            while (a > 0) {
-                if (a % 2 == 1) {
-                    count++;
-                }
-                a = a / 2;
-            }
-
-            bits[i] = count;
+        
+        
+        bits[0] = 0; 
+        
+        for (int i = 1; i <= n; i++) {
+            
+            bits[i] = bits[i >> 1] + (i & 1);
         }
-
+        
         return bits;
     }
 };
