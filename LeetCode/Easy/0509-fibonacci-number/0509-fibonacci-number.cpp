507d031bc713375b70b2637081e9df6c1b9f36f8
Devansh jain <devanshjaincampaign@gmail.com>
2026-07-17 12:33:01 +0530

    Time: 3 ms (65.74%), Space: 8.4 MB (11.18%) - LeetHub

--- LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
+++ LeetCode/Easy/0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 private:
-    int fibmemo(int n,vector<int>&memo){
-        if(n<=1){
-            return n;
-        }
-        if(memo[n]!=-1){
-            return memo[n];
-        }
-        memo[n]=fibmemo(n-1,memo)+fibmemo(n-2,memo);
+    int fibmemo(vector<int>& memo, int n){
+        if(n<=1)return n;
+
+        if(memo[n]!=-1)return memo[n];
+
+        memo[n]=fibmemo(memo,n-1)+fibmemo(memo,n-2);
         return memo[n];
     }
 public:
     int fib(int n) {
-        if(n<=1)return n;
         vector<int>memo(n+1,-1);
-        return fibmemo(n,memo);
+        int finalans=fibmemo(memo,n);
+        return finalans;
     }
 };
