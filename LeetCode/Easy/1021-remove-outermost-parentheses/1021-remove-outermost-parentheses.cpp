ce82a19ca09e8fe4a87a8f27353c36290271c36a
Devansh jain <devanshjaincampaign@gmail.com>
2026-07-20 18:06:41 +0530

    Time: 1 ms (24.81%), Space: 10.2 MB (8.82%) - LeetHub

---
+++ LeetCode/Easy/1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        stack<int>st;
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                st.push(s[i]);
+                if(st.size()>1){
+                    res+=s[i];
+                }
+            }else{
+                if(!st.empty() && st.top()=='('){
+                    st.pop();
+                    if(st.size()>=1){
+                        res+=')';
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
