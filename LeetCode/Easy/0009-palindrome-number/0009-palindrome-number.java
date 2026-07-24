6c47f7e5ac2fbaa06fe656aa7cf544d12bae6453
Devansh jain <devanshjaincampaign@gmail.com>
2026-07-24 16:22:16 +0530

    Time: 5 ms (80.95%), Space: 45.7 MB (94.57%) - LeetHub

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if ((x < 0) || (x % 10 == 0 && x != 0)) return false;
+        
+        int rev = 0;
+        int original = x; // Fixed spelling here
+
+        while (x != 0) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return original == rev;
+    }
+}
