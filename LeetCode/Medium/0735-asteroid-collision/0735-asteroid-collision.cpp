900d556be5837d44b0b3109a17236cb72c2df256
Devansh jain <devanshjaincampaign@gmail.com>
2026-07-26 07:37:38 +0530

    Time: 0 ms (100%), Space: 22.2 MB (19.55%) - LeetHub

---
+++ LeetCode/Medium/0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+
+        for (int i = 0; i < asteroids.size(); i++) {
+            bool destroyed = false;
+
+            while (!st.empty() && st.top() > 0 && asteroids[i] < 0) {
+                if (st.top() < abs(asteroids[i])) {
+                    st.pop();
+                } else if (st.top() == abs(asteroids[i])) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                st.push(asteroids[i]);
+            }
+        }
+
+        vector<int> vec;
+        while (!st.empty()) {
+            vec.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(vec.begin(), vec.end());
+
+        return vec;
+    }
+};
