75613796a70dfebeccc92091f7db253d41b8d29d
Devansh jain <devanshjaincampaign@gmail.com>
2026-08-06 14:36:58 +0530

    Time: 29 ms (41.18%), Space: 16 MB (96.73%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,34 +1,34 @@
 class Solution {
 private:
-    void dfs(vector<vector<char>>& grid, int r, int c, int rows, int cols) {
-        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] == '0') {
-            return;
-        }
+    void dfs(vector<vector<char>>&grid, int r, int c){
+        int row=grid.size();
+        int col=grid[0].size();
 
-        grid[r][c] = '0';
+        if(r<0||r>=row ||c<0||c>=col|| grid[r][c] == '0')return;
 
-        dfs(grid, r - 1, c, rows, cols);
-        dfs(grid, r + 1, c, rows, cols);
-        dfs(grid, r, c - 1, rows, cols);
-        dfs(grid, r, c + 1, rows, cols);
+        grid[r][c]='0';
+
+        dfs(grid,r+1,c);
+        dfs(grid,r-1,c);
+        dfs(grid,r,c+1);
+        dfs(grid,r,c-1);
     }
-
 public:
     int numIslands(vector<vector<char>>& grid) {
-        if (grid.empty()) return 0;
-        
-        int num_islands = 0;
-        int rows = grid.size();
-        int cols = grid[0].size();
+        if(grid.empty())return 0;
+        int islandcount=0;
 
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1') {
-                    num_islands++;
-                    dfs(grid, r, c, rows, cols);
+        int row=grid.size();
+        int col=grid[0].size();
+
+        for(int r=0;r<row;r++){
+            for(int c=0;c<col;c++){
+                if(grid[r][c]=='1'){
+                    islandcount++;
+                    dfs(grid,r,c);
                 }
             }
         }
-        return num_islands;
+        return islandcount;
     }
 };
