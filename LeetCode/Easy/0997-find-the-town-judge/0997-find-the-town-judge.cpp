e868db0c7beb2ffb36516125da4a299ccadf8e53
Devansh jain <devanshjaincampaign@gmail.com>
2026-08-06 10:10:02 +0530

    Time: 0 ms (100%), Space: 65.1 MB (65.54%) - LeetHub

--- LeetCode/Easy/0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ LeetCode/Easy/0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> net_trust(n + 1, 0);
-        
-        for (const auto& relationship : trust) {
-            net_trust[relationship[0]]--; // Out-degree
-            net_trust[relationship[1]]++; // In-degree
+        vector<int>trustscore(n+1,0);
+        for(const auto& relations:trust){
+            int u=relations[0];
+            int v=relations[1];
+
+            trustscore[u]--;
+            trustscore[v]++;
         }
-        
-        for (int i = 1; i <= n; i++) {
-            if (net_trust[i] == n - 1) {
+
+        for(int i=1;i<=n;i++){
+            if(trustscore[i]==n-1){
                 return i;
             }
         }
-        
         return -1;
     }
 };
