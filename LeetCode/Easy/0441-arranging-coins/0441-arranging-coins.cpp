26a03564cf036edc80bf16e741590d49c8651f6c
Devansh jain <devanshjaincampaign@gmail.com>
2026-08-06 07:44:15 +0530

    Time: 6 ms (16.6%), Space: 8.7 MB (68.87%) - LeetHub

---
+++ LeetCode/Easy/0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+            if(sum>n){
+                return i-1;
+            }
+            
+        }
+        return sum;
+    }
+};
