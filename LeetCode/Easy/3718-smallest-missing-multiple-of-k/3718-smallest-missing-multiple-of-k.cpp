052dc44c8a19766c859107bc410fd3cce3f7e769
Devansh jain <devanshjaincampaign@gmail.com>
2026-08-25 09:27:33 +0530

    Time: 6 ms (12.42%), Space: 25.3 MB (24.36%) - LeetHub

--- LeetCode/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ LeetCode/Easy/3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -5,10 +5,10 @@ public:
         
         unordered_set<int> st(nums.begin(), nums.end());
         
-        // At most nums.size() + 1 checks are needed
+        
         for (long long i = 1; ; ++i) {
             long long multiple = i * k;
-            if (multiple > INT_MAX) return -1; // overflow guard
+            if (multiple > INT_MAX) return -1; 
             
             if (st.find(static_cast<int>(multiple)) == st.end()) {
                 return static_cast<int>(multiple);
