c78eefc9f71b5c2bf5d5a94719ee565404711462
Devansh jain <devanshjaincampaign@gmail.com>
2026-09-05 15:25:13 +0530

    Time: 0 ms (100%), Space: 12.7 MB (12.63%) - LeetHub

---
+++ LeetCode/Easy/1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int>prefsum(nums.size(),0);
+        prefsum[0]=nums[0];
+        for(int i=1;i<nums.size();i++){
+            prefsum[i]=prefsum[i-1]+nums[i];
+        }
+
+        return prefsum;
+    }
+};
